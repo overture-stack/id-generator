6311b5185503890dd592ddc1ad1df37fab4183ae
UmmulkiramR <urangwala@oicr.on.ca>
2024-01-07 14:45:44 -0500

    added support for environment variables.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,15 @@
 
+import * as dotenv from "dotenv";
+
+const config = dotenv.config();
+if(config.error){
+    console.log(`Error loading environment variables, aborting.`);
+    process.exit(1);
+}
+
+const port = process.env.PORT
+
+
 import * as express from 'express';
 import {root} from "./routes/root";
 
@@ -9,17 +20,17 @@ const app = express();
 
 function setExpress(){
 
-    app.route("/").get(root)
+    app.route("/").get(root);
 
 }
 
 
 
 function startServer(){
-    app.listen(9000, () => {
-            console.log(`HTTP REST API SERVER available at http://localhost:9000`);
+    app.listen(port, () => {
+            console.log(`HTTP REST API SERVER available at http://localhost:${port}`);
     });
 }
 
-setExpress()
-startServer()
+setExpress();
+startServer();
