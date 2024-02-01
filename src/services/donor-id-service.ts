7c40305a02cdf7eb97575ac6f806b883756714a0
UmmulkiramR <urangwala@oicr.on.ca>
2024-01-31 19:49:28 -0500

    comments

--- src/services/donor-id-service.ts
+++ src/services/donor-id-service.ts
@@ -6,13 +6,14 @@ import {Donor} from "../models/donor";
 
 const donorRepo = AppDataSource.getRepository(Donor);
 
- async function createId(programId: string, submitterId: string ){
 // create donor entry
+ async function createId(programId: string, submitterId: string ){
     const donor = donorRepo.create(new Donor(submitterId, programId));
     const savedDonor = await donorRepo.save(donor);
     return savedDonor.donorId;
 }
 
+// check if a donor id exists for the submitter and program
  async function getDonorId(programId: string, submitterId: string){
      const donor = await donorRepo.createQueryBuilder("donors")
          .where("donors.submitter_id = :subId", {subId: submitterId})
@@ -23,7 +24,7 @@ const donorRepo = AppDataSource.getRepository(Donor);
 
 
 export async function getId(request: Request, response: Response){
- // check if a donor id exists for the submitter and program
+
     const programId = request.params.programId;
     const submitterId  = request.params.submitterId;
 
